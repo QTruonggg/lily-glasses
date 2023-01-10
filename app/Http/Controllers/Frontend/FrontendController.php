4f69192609be4f1ce789d23a5a36b460dd85f89f
linh2000 <ngoquanglinh23062000@gmail.com>
2023-01-10 14:19:08 +0700

    linh

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Hook\CategoryHook;
 
 class FrontendController extends Controller
 {
-    public function showHome() {
+    public function showHome(CategoryHook $CategoryHook) {
+        // $parent_categories = $CategoryHook->getParentStatus();
         return view('frontend.components.master'); 
     }
    
---
+++ resources/views/frontend/components/header.blade.php
@@ -0,0 +1,41 @@
+<section id="header">
+    <header class="main-header-mobile">
+        <div class="container">
+            <div class="row">
+                <div class="col-2">
+                    <div class="mobile-sidebar">
+                        <a class="" data-bs-toggle="offcanvas" href="#mb-menu-sidebar" role="button" aria-controls="mb-menu-sidebar">
+                            <i class="fa-solid fa-bars"></i>
+                        </a>
+                        <div class="offcanvas offcanvas-start" tabindex="-1" id="mb-menu-sidebar" aria-labelledby="offcanvasExampleLabel">
+                        <div class="offcanvas-header">
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <ul class="mb-sidebar-list">
+                                <li>
+                                    <ul class="mb-sidebar-list-child">
+                                        <li>
+
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                                
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    asdfda
+                </div>
+                <div class="col-2">
+                    asdf
+                </div>
+                <div class="col-2">
+                    asdf
+                </div>
+            </div>
+        </div>
+    </header>
+</section>
