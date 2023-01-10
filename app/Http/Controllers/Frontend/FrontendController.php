50e7446d468de8a37e0473c7b41c31e907a6e749
linh2000 <ngoquanglinh23062000@gmail.com>
2023-01-10 14:27:07 +0700

    Merge branch 'dev' of https://github.com/QTruonggg/lily-glasses into dev

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -3,24 +3,14 @@
 namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-<<<<<<< HEAD
 use App\Http\Controllers\Hook\CategoryHook;
-
+use App\models\Banner;
 class FrontendController extends Controller
 {
-    public function showHome(CategoryHook $CategoryHook) {
-        // $parent_categories = $CategoryHook->getParentStatus();
-        return view('frontend.components.master'); 
-=======
-use App\Models\Banner;
-
-
-class FrontendController extends Controller
-{
-    public function showHome(){
+   public function showHome(){
         $banner = Banner::all();
         return view('frontend.home.index',
         compact('banner'));
->>>>>>> 1e54f4fc0dd86864ca75cdf03b4c405f8bff1e4a
     }
+   
 }
