df9699b7b334749db76116b11b96d6a0059ab30f
linh2000 <ngoquanglinh23062000@gmail.com>
2023-02-01 13:53:43 +0700

    no message

--- resources/views/frontend/components/master.blade.php
+++ resources/views/frontend/components/master.blade.php
@@ -52,6 +52,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <link rel="stylesheet" href="{{asset('fontawesome/css/all.min.css')}}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="shortcut icon" href="{{asset('assets/favicon.ico')}}">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300&family=Noto+Sans+Limbu&family=Roboto+Condensed&display=swap" rel="stylesheet">
     <style> @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300&family=Noto+Sans+Limbu&family=Roboto+Condensed&display=swap'); </style>
